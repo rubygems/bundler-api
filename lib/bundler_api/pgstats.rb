1c702bc094ffdfe76d0711c136b56e8ec33c2aaa
Larry Marburger <larry@marburger.cc>
2012-10-24 11:27:31 -0400

    round db stats measure time down to the minute

--- lib/bundler_api/pgstats.rb
+++ lib/bundler_api/pgstats.rb
@@ -1,11 +1,11 @@
 require 'librato/metrics'
-require_relative 'metriks'
 
 class PGStats
-  def initialize(db, client = Librato::Metrics.client)
+  def initialize(db, counters = Hash.new, interval = 60, client = Librato::Metrics.client)
     @db       = db
+    @counters = counters
+    @interval = interval
     @client   = client
-    @counters = Hash.new
   end
 
   def stats
@@ -20,19 +20,26 @@ class PGStats
   end
 
   def submit
-    queue = @client.new_queue
+    queue        = @client.new_queue
+    measure_time = now_floored
 
     stats.each do |name, current_counter|
-      current_counter = current_counter.to_f
+      current_counter = current_counter.to_i
       last_counter    = @counters[name]
       if last_counter && current_counter >= last_counter
         value = current_counter - last_counter
-        queue.add("postgres.#{name}" => value)
+        queue.add("postgres.#{name}" => { :value        => value,
+                                          :measure_time => measure_time })
       end
 
       @counters[name] = current_counter
     end
 
     queue.submit unless queue.empty?
+  end
+
+  def now_floored
+    time = Time.now.to_i
+    time - (time % @interval)
   end
 end
