89286c2c56167a5b418f980fa9472b1550cf4a54
Terence Lee <hone02@gmail.com>
2012-10-31 17:30:11 -0500

    test to ensure the ConsumerPool executes jobs concurrently

--- spec/update/consumer_pool_spec.rb
+++ spec/update/consumer_pool_spec.rb
@@ -1,3 +1,5 @@
+require 'monitor'
+require 'timeout'
 require_relative '../spec_helper'
 require_relative '../../lib/bundler_api/update/consumer_pool'
 
@@ -11,6 +13,57 @@ describe BundlerApi::ConsumerPool do
 
     def run
       @@counter += 1
+    end
+  end
+
+  class Latch
+    def initialize(count = 1)
+      @monitor = Monitor.new
+      @cv      = @monitor.new_cond
+      @count   = count
+    end
+
+    def wait
+      @monitor.synchronize do
+        @cv.wait_until { @count > 0 }
+      end
+    end
+
+    def release
+      @monitor.synchronize do
+        @count -= 1 if @count > 0
+        @cv.broadcast if @count.zero?
+      end
+    end
+  end
+
+  class FirstJob
+    attr_reader :ran
+
+    def initialize(test_latch, job_latch)
+      @test_latch = test_latch
+      @job_latch  = job_latch
+      @ran        = false
+    end
+
+    def run
+      @test_latch.release
+      @job_latch.wait
+      @ran = true
+    end
+  end
+
+  class SecondJob
+    attr_reader :ran
+
+    def initialize(latch)
+      @latch = latch
+      @ran   = false
+    end
+
+    def run
+      @latch.release
+      @ran = true
     end
   end
 
@@ -32,5 +85,26 @@ describe BundlerApi::ConsumerPool do
     pool.join
 
     expect(TestJob.counter).to eq(1)
+  end
+
+  it "works concurrently" do
+    job_latch  = Latch.new
+    test_latch = Latch.new
+    pool  = BundlerApi::ConsumerPool.new(2)
+    job1  = FirstJob.new(test_latch, job_latch)
+    job2  = SecondJob.new(job_latch)
+
+    pool.start
+    pool.enq(job1)
+    # ensure the first job is executing
+    # requires second job to finish
+    Timeout.timeout(1) { test_latch.release }
+    pool.enq(job2)
+    pool.poison
+
+    pool.join
+
+    expect(job1.ran).to be_true
+    expect(job2.ran).to be_true
   end
 end
