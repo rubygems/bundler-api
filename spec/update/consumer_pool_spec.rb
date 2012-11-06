580226128cd9c3603422cb9af2eb20e607692216
Terence Lee <hone02@gmail.com>
2012-11-06 12:15:15 -0800

    extract out latch

--- spec/update/consumer_pool_spec.rb
+++ spec/update/consumer_pool_spec.rb
@@ -16,27 +16,6 @@ describe BundlerApi::ConsumerPool do
     end
   end
 
-  class Latch
-    def initialize(count = 1)
-      @monitor = Monitor.new
-      @cv      = @monitor.new_cond
-      @count   = count
-    end
-
-    def wait
-      @monitor.synchronize do
-        @cv.wait_until { @count > 0 }
-      end
-    end
-
-    def release
-      @monitor.synchronize do
-        @count -= 1 if @count > 0
-        @cv.broadcast if @count.zero?
-      end
-    end
-  end
-
   class FirstJob
     attr_reader :ran
 
