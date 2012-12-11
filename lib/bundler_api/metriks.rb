09efd1bdd56a7ea3b0c1bc9de67b754b2a121c28
Andre Arko <andre@arko.net>
2012-12-11 12:28:11 -0800

    track old and new librato metrics for now

--- lib/bundler_api/metriks.rb
+++ lib/bundler_api/metriks.rb
@@ -1,4 +1,5 @@
 require 'metriks'
+require 'metriks/middleware'
 
 user  = ENV['LIBRATO_METRICS_USER']
 token = ENV['LIBRATO_METRICS_TOKEN']
@@ -7,14 +8,27 @@ if user && token
   require 'socket'
 
   source   = Socket.gethostname
+  prefix   = ENV['RACK_ENV'] || 'development'
   on_error = ->(e) do STDOUT.puts("LibratoMetrics: #{ e.message }") end
   Metriks::Reporter::LibratoMetrics.new(user, token,
-                                        on_error: on_error,
-                                        source:   source).start
+    on_error: on_error,
+    source:   source,
+    prefix: prefix
+  ).start
+
+  old_user  = ENV['OLD_LIBRATO_METRICS_USER']
+  old_token = ENV['OLD_LIBRATO_METRICS_TOKEN']
+  if old_user && old_token
+    Metriks::Reporter::LibratoMetrics.new(old_user, old_token,
+      on_error: on_error,
+      source:   source,
+    ).start
+  end
 else
   require 'metriks/reporter/logger'
-  Metriks::Reporter::Logger.new(logger:   Logger.new("/dev/null"),
-                                interval: 10).start
+  Metriks::Reporter::Logger.new(
+    logger: Logger.new("/dev/null"),
+    interval: 10
+  ).start
 end
 
-require 'metriks/middleware'
