df2b8a1e0c14fcd1df1d2de2c0946916733ae498
Larry Marburger <larry@marburger.cc>
2013-02-05 10:26:21 -0500

    Add optional prefix to librato metrics

--- lib/bundler_api/metriks.rb
+++ lib/bundler_api/metriks.rb
@@ -6,11 +6,16 @@ token = ENV['LIBRATO_METRICS_TOKEN']
 if user && token
   require 'metriks/reporter/librato_metrics'
   require 'socket'
+
+  prefix = ENV.fetch('LIBRATO_METRICS_PREFIX') do
+    ENV['RACK_ENV'] unless ENV['RACK_ENV'] == 'production'
+  end
+
   source   = Socket.gethostname
-  prefix   = ENV['RACK_ENV'] || 'development'
   on_error = ->(e) do STDOUT.puts("LibratoMetrics: #{ e.message }") end
-  opts = { on_error: on_error, source: source }
-  opts.merge!(prefix: prefix) unless prefix == "production"
+  opts     = { on_error: on_error, source: source }
+  opts[:prefix] = prefix if prefix && !prefix.empty?
+
   Metriks::Reporter::LibratoMetrics.new(user, token, opts).start
 else
   require 'metriks/reporter/logger'
