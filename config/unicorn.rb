c506be970ee6152ac908f369cc513f52f6781c36
Larry Marburger <larry@marburger.cc>
2013-02-15 11:42:09 -0500

    Don't preload the app for now

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -42,9 +42,12 @@ timeout 30
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
-preload_app true
 # GC.respond_to?(:copy_on_write_friendly=) and
 #   GC.copy_on_write_friendly = true
+#
+# Setting to false until the app is configured to setup all its connections
+# (Sequel, Metrics, etc.) after forking.
+preload_app false
 
 # Enable this flag to have unicorn test client connections by writing the
 # beginning of the HTTP headers before calling the application.  This
@@ -91,8 +94,8 @@ after_fork do |server, worker|
   # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
 
   # the following is *required* for Rails + "preload_app true",
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  # defined?(ActiveRecord::Base) and
+  #   ActiveRecord::Base.establish_connection
 
   # if preload_app is true, then you may also want to check and
   # restart any other shared sockets/descriptors such as Memcached,
