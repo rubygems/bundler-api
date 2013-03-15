409d6d563b44fd81f05b97c3b8f2d96299ca17d4
Larry Marburger <larry@marburger.cc>
2013-03-15 19:34:29 -0400

    Use process name in metric source

--- lib/bundler_api/metriks.rb
+++ lib/bundler_api/metriks.rb
@@ -5,13 +5,18 @@ user  = ENV['LIBRATO_METRICS_USER']
 token = ENV['LIBRATO_METRICS_TOKEN']
 if user && token
   require 'metriks/reporter/librato_metrics'
-  require 'socket'
 
   prefix = ENV.fetch('LIBRATO_METRICS_PREFIX') do
     ENV['RACK_ENV'] unless ENV['RACK_ENV'] == 'production'
   end
 
-  source   = [ Socket.gethostname, Process.pid ].join('-')
+  app_name = ENV.fetch('PS') do
+    # Fall back to hostname if PS isn't set.
+    require 'socket'
+    Socket.gethostname
+  end
+
+  source   = [ app_name, Process.pid ].join('-')
   on_error = ->(e) do STDOUT.puts("LibratoMetrics: #{ e.message }") end
   opts     = { on_error: on_error, source: source }
   opts[:prefix] = prefix if prefix && !prefix.empty?
