0090e0e4430b3a5572d4bdb98aaa669200de1a85
Terence Lee <hone02@gmail.com>
2013-04-19 18:47:40 -0700

    add debugging

--- lib/bundler_api/rack_queue_metrics_reporter.rb
+++ lib/bundler_api/rack_queue_metrics_reporter.rb
@@ -7,16 +7,24 @@ module Rack
       def setup_queue_depth
         ActiveSupport::Notifications.subscribe("rack.queue-metrics.queue-depth") do |*args|
           _, _, _, _, payload = args
+          begin
           Metriks.histogram(instrument_name('queue-depth')).update(payload[:requests][:queued])
           Metriks.histogram(instrument_name('active-requests')).update(payload[:requests][:active])
+          rescue StandardError => e
+            $stderr.puts e
+          end
         end
       end
 
       def setup_queue_time
         ActiveSupport::Notifications.subscribe("rack.queue-metrics.queue-time") do |*args|
           _, _, _, _, payload = args
+          begin
           if value = payload[:request_start_delta]
             Metriks.histogram(instrument_name('request_start_delta')).update(value)
+          end
+          rescue StandardError => e
+            $stderr.puts e
           end
         end
       end
@@ -24,10 +32,14 @@ module Rack
       def setup_app_time
         ActiveSupport::Notifications.subscribe("rack.queue-metrics.app-time") do |*args|
           _, _, _, _, payload = args
+          begin
           [:app_delta, :middleware_delta].each do |key|
             if value = payload[key]
               Metriks.histogram(instrument_name(key.to_s)).update(value)
             end
+          end
+          rescue StandardError => e
+            $stderr.puts e
           end
         end
       end
