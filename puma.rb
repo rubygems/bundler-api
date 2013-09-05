fe2140a7eb471e204f3ead168e9ad785d99b8dae
Larry Marburger <larry@marburger.cc>
2013-09-05 14:44:53 -0400

    move env setup out of puma configuration

--- puma.rb
+++ puma.rb
@@ -1,8 +1,2 @@
-lib = File.expand_path('../lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-
-require 'bundler/setup'
-require 'bundler_api/env'
-
 threads ENV.fetch('MIN_THREADS', 0), ENV.fetch('MAX_THREADS', 1)
 port ENV['PORT'] if ENV['PORT']
