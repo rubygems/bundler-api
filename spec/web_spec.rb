9603ecf21b20779b2ddf3e30324b9b41b1a07529
Terence Lee <hone02@gmail.com>
2013-10-05 20:59:39 -0400

    use database for full integration tests in the web_spec

--- spec/web_spec.rb
+++ spec/web_spec.rb
@@ -5,47 +5,14 @@ require 'bundler_api/web'
 describe BundlerApi::Web do
   include Rack::Test::Methods
 
-  class MockSequel
-    @@rack_triple = [{
-      name:     "rack",
-      number:   "1.0.0",
-      platform: "ruby",
-      dep_name: nil
-    }]
-
-    def [](*args)
-      case args.first
-      when :rubygems, :versions
-        @select = (@full ? [{:id => 1}] : [])
-        @first = nil
-        self
-      when :dependencies
-        @select = [{:id => 1}]
-        @first = {:requirements => ">= 1.0"}
-        self
-      else
-        args[2] == "1.0.1" ? [] : @@rack_triple
-      end
-    end
-
-    attr_reader :whered, :filtered, :selected, :inserted, :updated
-    attr_accessor :full
-    def where(*args); @whered ||= []; @whered << args; self; end
-    def filter(*args); @filtered ||= []; @filtered << args; self; end
-    def select(*args); @selected ||= []; @selected << args; @select; end
-    def insert(*args); @inserted ||= []; @inserted << args; true; end
-    def update(*args); @updated ||= []; @updated << args; true; end
-    def first; @first; end
-    def transaction; yield; end
-  end
-
   before do
-    @read_db  = MockSequel.new
-    @write_db = MockSequel.new
+    builder = GemBuilder.new($db)
+    rack_id = builder.create_rubygem("rack")
+    builder.create_version(rack_id, "rack")
   end
 
   def app
-    BundlerApi::Web.new(@read_db, @write_db)
+    BundlerApi::Web.new($db, $db)
   end
 
   context "GET /" do
@@ -145,9 +112,6 @@ describe BundlerApi::Web do
     it "adds the spec to the database" do
       post url, JSON.dump(payload)
 
-      expect(@write_db.inserted[0].first[:name]).to eq("rack")
-      expect(@write_db.inserted[1].first[:full_name]).to eq("rack-1.0.1")
-
       expect(last_response).to be_ok
       res = JSON.parse(last_response.body)
       expect(res["name"]).to eq("rack")
@@ -160,14 +124,8 @@ describe BundlerApi::Web do
     let(:payload){ {:name => "rack", :version => "1.0.0",
       :platform => "ruby", :prerelease => false} }
 
-    before do
-      @write_db.full = true
-    end
-
     it "removes the spec from the database" do
       post url, JSON.dump(payload)
-
-      expect(@write_db.updated[0].first[:indexed]).to eq(false)
 
       expect(last_response).to be_ok
       res = JSON.parse(last_response.body)
