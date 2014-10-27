ed2fbbdc41ccf7d86db6643c24f0fc08b51b6017
Andre Medeiros <me@andremedeiros.info>
2014-10-27 08:21:34 +0100

    Split the matcher into its own file.

--- spec/agent_reporting_spec.rb
+++ spec/agent_reporting_spec.rb
@@ -1,16 +1,6 @@
 require 'spec_helper'
 require 'bundler_api/agent_reporting'
 
-RSpec::Matchers.define :be_incremented_for do |expected|
-  match do |actual|
-    actual.values[expected] > 0
-  end
-
-  failure_message_for_should do |actual|
-    "expected '#{ expected }' to be incremented, but it wasn't"
-  end
-end
-
 describe BundlerApi::AgentReporting do
   class FakeMetriks
     attr_accessor :values, :key
