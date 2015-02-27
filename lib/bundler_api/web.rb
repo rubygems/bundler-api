00ec97d0256e086ede8ff6e68e2596b21c6a3dac
Arthur Neves <arthurnn@gmail.com>
2015-02-27 14:31:18 -0500

    use memcachier on production

--- lib/bundler_api/web.rb
+++ lib/bundler_api/web.rb
@@ -40,7 +40,18 @@ class BundlerApi::Web < Sinatra::Base
                      max_connections: ENV['MAX_THREADS'])
     end
 
-    @dalli_client = Dalli::Client.new
+    @dalli_client =
+      if ENV["MEMCACHIER_SERVERS"]
+        Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                          { username: ENV["MEMCACHIER_USERNAME"],
+                            password: ENV["MEMCACHIER_PASSWORD"],
+                            failover: true,
+                            socket_timeout: 1.5,
+                            socket_failure_delay: 0.2
+                          })
+      else
+        Dalli::Client.new
+      end
     super()
   end
 
