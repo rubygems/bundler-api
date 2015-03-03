e0d465f3b0da56bee422e7fbd77faee0021cc6bd
Arthur Neves <arthurnn@gmail.com>
2015-03-03 08:51:12 -0500

    Fix rescue syntax

--- lib/bundler_api/agent_reporting.rb
+++ lib/bundler_api/agent_reporting.rb
@@ -46,7 +46,7 @@ private
 
     BundlerApi.redis.setex(id, 120, true)
     false
-  rescue ex
+  rescue => ex
     # Sometimes we get these, and there's no point throwing out a perfectly
     # good metric. We still, however, want to report it in Appsignal so that
     # we know what's up.
