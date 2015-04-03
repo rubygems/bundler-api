e3aaed6a011fadbb73dba9b3fcbe3d3c6f0fd69a
Arthur Neves <arthurnn@gmail.com>
2015-04-03 18:58:01 -0400

    Add default ttl, 15 min to gem cache
    Not sure how, but we are getting some gem cache out-of-sync.
    Probably some race condition, or a something else.
    If we add a ttl, it will fix the issue, even if that doesnt make me happy.
    
    We can play if the TTL, after we deploy this, we can watch the number of cache miss,
    and lower or increase this number

--- lib/bundler_api/cache.rb
+++ lib/bundler_api/cache.rb
@@ -69,7 +69,8 @@ module BundlerApi
                              password: ENV["MEMCACHIER_PASSWORD"],
                              failover: true,
                              socket_timeout: 1.5,
-                             socket_failure_delay: 0.2
+                             socket_failure_delay: 0.2,
+                             expires_in: 15 * 60
                             })
         else
           Dalli::Client.new
