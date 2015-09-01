483d8c7674c61f6f4c680e5176792b3365adb1c3
Andre Arko <andre@arko.net>
2015-09-01 12:28:37 -0700

    handle errors during error reporting

--- lib/bundler_api/metriks.rb
+++ lib/bundler_api/metriks.rb
@@ -18,9 +18,14 @@ module BundlerApi
       end
 
       on_error = ->(e) do
-        STDOUT.puts("LibratoMetrics error. The response from the server was #{e.message} #{e.res.body}")
-        STDOUT.puts("The data we sent was: #{e.data.inspect}")
-        STDOUT.puts(e.backtrace)
+        begin
+          STDOUT.puts("[Metriks][Librato] Error while submitting metrics: #{e.message}")
+          STDOUT.puts("  #{e.res.body}")
+          STDOUT.puts("  Submitted data: #{e.data.inspect}")
+        rescue => e
+          STDOUT.puts "#{e.class}: #{e.message}"
+          STDOUT.puts e.backtrace.join("\n  ")
+        end
       end
 
       opts = { on_error: on_error, source: app_name }
