7bc664f35d5007cbd2121f8d01b05ee7f67bdff5
Andre Arko <andre@arko.net>
2015-09-01 02:05:39 -0700

    run one librato reporter in each worker

--- lib/bundler_api/metriks.rb
+++ lib/bundler_api/metriks.rb
@@ -1,30 +1,42 @@
 require 'metriks'
 require 'metriks/middleware'
+require 'metriks-librato_metrics'
 
-user  = ENV['LIBRATO_METRICS_USER']
-token = ENV['LIBRATO_METRICS_TOKEN']
-if user && token
-  require 'metriks-librato_metrics'
+module BundlerApi
+  module Metriks
+    def self.start
+      return unless user && token
 
-  prefix = ENV.fetch('LIBRATO_METRICS_PREFIX') do
-    ENV['RACK_ENV'] unless ENV['RACK_ENV'] == 'production'
+      prefix = ENV.fetch('LIBRATO_METRICS_PREFIX') do
+        ENV['RACK_ENV'] unless ENV['RACK_ENV'] == 'production'
+      end
+
+      app_name = ENV.fetch('DYNO') do
+        # Fall back to hostname if DYNO isn't set.
+        require 'socket'
+        Socket.gethostname
+      end
+
+      on_error = ->(e) do
+        STDOUT.puts("LibratoMetrics error. The response from the server was #{e.message} #{e.res.body}")
+        STDOUT.puts("The data we sent was: #{e.data.inspect}")
+        STDOUT.puts(e.backtrace)
+      end
+
+      opts = { on_error: on_error, source: app_name }
+      opts[:prefix] = prefix if prefix && !prefix.empty?
+
+      ::Metriks::LibratoMetricsReporter.new(user, token, opts).start
+    end
+
+    def self.user
+      ENV['LIBRATO_METRICS_USER']
+    end
+
+    def self.token
+      ENV['LIBRATO_METRICS_TOKEN']
+    end
+
   end
-
-  app_name = ENV.fetch('DYNO') do
-    # Fall back to hostname if DYNO isn't set.
-    require 'socket'
-    Socket.gethostname
-  end
-
-  on_error = ->(e) do
-    STDOUT.puts("LibratoMetrics error. The response from the server was #{e.message} #{e.res.body}")
-    STDOUT.puts("The data we sent was: #{e.data.inspect}")
-    STDOUT.puts(e.backtrace)
-  end
-
-  opts = { on_error: on_error, source: app_name }
-  opts[:prefix] = prefix if prefix && !prefix.empty?
-
-  Metriks::LibratoMetricsReporter.new(user, token, opts).start
 end
 
--- puma.rb
+++ puma.rb
@@ -1,3 +1,10 @@
-threads ENV.fetch('MIN_THREADS', 0), ENV.fetch('MAX_THREADS', 1)
+threads ENV.fetch('MIN_THREADS', 1), ENV.fetch('MAX_THREADS', 1)
 port ENV['PORT'] if ENV['PORT']
-workers ENV.fetch('WORKER_COUNT', 0)
+workers ENV.fetch('WORKER_COUNT', 1)
+
+require 'bundler_api/metriks'
+BundlerApi::Metriks.start
+
+on_worker_boot do
+  BundlerApi::Metriks.start
+end
