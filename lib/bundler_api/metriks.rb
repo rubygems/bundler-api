d6b1b079a2e422ed43d68fc7c921bd3234ec0570
Andre Arko <andre@arko.net>
2015-09-01 19:43:43 -0700

    extract methods to make rubocop happy

--- lib/bundler_api/metriks.rb
+++ lib/bundler_api/metriks.rb
@@ -5,20 +5,36 @@ require 'metriks-librato_metrics'
 module BundlerApi
   module Metriks
     def self.start(worker_index = nil)
+      new(ENV['LIBRATO_METRICS_USER'], ENV['LIBRATO_METRICS_TOKEN'], worker_index).start
+    end
+
+    def initialize(user, token, worker_index = nil)
       return unless user && token
+
+      opts = { on_error: error_handler, source: source_name(worker_index) }
 
       prefix = ENV.fetch('LIBRATO_METRICS_PREFIX') do
         ENV['RACK_ENV'] unless ENV['RACK_ENV'] == 'production'
       end
+      opts[:prefix] = prefix if prefix && !prefix.empty?
 
-      source_name = ENV.fetch('DYNO') do
+      ::Metriks::LibratoMetricsReporter.new(user, token, opts).start
+    end
+
+  private
+
+    def source_name(worker = nil)
+      name = ENV.fetch('DYNO') do
         # Fall back to hostname if DYNO isn't set.
         require 'socket'
         Socket.gethostname
       end
-      source_name += ".w#{worker_index}" if worker_index
 
-      on_error = -> (e) do
+      worker ? "#{name}.w#{worker}" : name
+    end
+
+    def error_handler
+      -> (e) do
         STDOUT.puts("[Metriks][Librato] #{e.class} raised during metric submission: #{e.message}")
 
         if e.is_a?(::Metriks::LibratoMetricsReporter::RequestFailedError)
@@ -28,19 +44,6 @@ module BundlerApi
           STDOUT.puts e.backtrace.join("\n  ")
         end
       end
-
-      opts = { on_error: on_error, source: source_name }
-      opts[:prefix] = prefix if prefix && !prefix.empty?
-
-      ::Metriks::LibratoMetricsReporter.new(user, token, opts).start
-    end
-
-    def self.user
-      ENV['LIBRATO_METRICS_USER']
-    end
-
-    def self.token
-      ENV['LIBRATO_METRICS_TOKEN']
     end
   end
 end
