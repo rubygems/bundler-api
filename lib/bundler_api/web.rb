d497c0ec5cbdaf7ac93fc4e8970550d7f4a1ce5d
Samuel Giddins <segiddins@segiddins.me>
2016-04-19 15:22:42 -0500

    Do cache purging in the background

--- lib/bundler_api/web.rb
+++ lib/bundler_api/web.rb
@@ -123,8 +123,10 @@ class BundlerApi::Web < Sinatra::Base
       job = BundlerApi::Job.new(@write_conn, payload)
       job.run
 
-      @cache.purge_specs
-      @cache.purge_gem(payload.name)
+      Thread.new do
+        @cache.purge_specs
+        @cache.purge_gem(payload.name)
+      end
 
       json_payload(payload)
     end
@@ -140,8 +142,10 @@ class BundlerApi::Web < Sinatra::Base
         platform:   payload.platform
       ).update(indexed: false)
 
-      @cache.purge_specs
-      @cache.purge_gem(payload.name)
+      Thread.new do
+        @cache.purge_specs
+        @cache.purge_gem(payload.name)
+      end
 
       json_payload(payload)
     end
